66d8f836acb2cfae5e95a508f37a353775b1902f
Mathimugan <68652175+Mathimugan@users.noreply.github.com>
2025-04-24 18:32:53 +0800

    Updated Kanbanstore file path in the task.vue file

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -78,8 +78,8 @@ import {
 import Modal from "./common/Modal.vue";
 import TaskForm from "./TaskForm.vue";
 import { ref } from "vue";
-import kanbanStore from "../stores/Kanbanstore";
-import DropZone from "./common/DropZone.vue"
+import kanbanStore from "../stores/KanbanStore";
+import DropZone from "./common/DropZone.vue";
 import Draggable from "./common/Draggable.vue";
 import { X , Pencil} from 'lucide-vue-next'
 
