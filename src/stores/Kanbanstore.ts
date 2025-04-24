9066c97ebbd5c0c0b4d2da0ba087c5910ab26e0f
Mathimugan <68652175+Mathimugan@users.noreply.github.com>
2025-04-24 17:59:30 +0800

    removed the onAuthStateChanged method in kanbanstore.ts

--- src/stores/Kanbanstore.ts
+++ src/stores/Kanbanstore.ts
@@ -1,9 +1,8 @@
 import {db} from "../firebase"
-import { ref } from "vue";
 import { v4 as uuidv4 } from "uuid";
 import { useLocalStorage } from "@vueuse/core";
 import { collection,getDoc, getDocs,addDoc,updateDoc,doc,arrayRemove, arrayUnion,deleteDoc,onSnapshot } from "firebase/firestore";
-import { getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,onAuthStateChanged,signOut } from "firebase/auth";
+import { getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,signOut } from "firebase/auth";
 import type { Column, Task,Registration } from "@/types";
 const auth = getAuth();
 let KEY = "KANBAN-STORE";
